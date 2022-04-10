181366aa481cda77c9a0248bf7bf82375cfb7386
Boris <borislocal@yandex.ru>
2022-04-10 03:30:02 +0300

    fixed loop condition to break on first difference

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,10 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            result = pref[i] == word[i];
+            if (pref[i] != word[i]) {
+                result = false;
+                break;
+            }
         }
         return result;
     }
